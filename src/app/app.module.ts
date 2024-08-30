2db57a3af8a54b234f60a4dd80606358f2b9ecc5
edwinkay <edwinkaycut@gmail.com>
2024-08-30 11:04:33 -0500

    todo-list add material pt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
+//modulos de material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [AppComponent, TodoListComponent],
   imports: [
@@ -17,6 +26,13 @@ import { TodoListComponent } from './todo-list/todo-list.component';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
